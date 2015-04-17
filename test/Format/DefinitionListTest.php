bdade45664e8dd51847ead56e23918321d540271
Isaac Leinweber <brotchen@gmail.com>
2015-04-17 01:18:47 +0000

    Testing the definition list.

--- test/Format/DefinitionListTest.php
+++ test/Format/DefinitionListTest.php
@@ -2,6 +2,30 @@
 
 namespace kilahm\Clio\Test\Format;
 
+use kilahm\Clio\Format\DefinitionList;
+
 class DefinitionListTest extends \HackPack\HackUnit\Core\TestCase
 {
+    private static Map<string,string> $data = Map{
+        'Item 1' => 'Describes item 1.  Item one is the most A+ type item!',
+        'Item B' => 'Meh...',
+        'Third Item' => '3 lord.',
+        'Item the fourth' => '4 shalt thou not count.  5 is right out!',
+    };
+
+    public function testDefListWillRender() : void
+    {
+        $this->expectCallable(() ==> {
+            DefinitionList::make(self::$data)->render();
+        })->toNotThrow();
+    }
+
+    public function testDefListContainsAllData() : void
+    {
+        $list = DefinitionList::make(self::$data)->render();
+        foreach(self::$data as $term => $description) {
+            $this->expect($list)->toMatch(sprintf('#%s#', $term));
+            $this->expect($list)->toMatch(sprintf('#%s#', $description));
+        }
+    }
 }
